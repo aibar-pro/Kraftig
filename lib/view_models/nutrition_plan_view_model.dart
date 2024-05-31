523eb4a8c3f283b730cde98ae288ef845557eed9
Aleksei Baranchikov <job@aibar.pro>
2024-05-31 21:07:20 +0500

    Added login check before plan request

--- lib/view_models/nutrition_plan_view_model.dart
+++ lib/view_models/nutrition_plan_view_model.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 import '../models/plan_model.dart';
+import '../models/user_profile_model.dart';
+import 'home_view_model.dart';
 
 class NutritionPlanViewModel extends ChangeNotifier {
+  final HomeViewModel homeViewModel;
+
   PlanState state = PlanState.invitation;
   Plan? activePlan;
+
+  NutritionPlanViewModel({required this.homeViewModel});
+
+  UserProfileModel? get userProfile => homeViewModel.userProfile;
 
   void requestNewPlan() {
     state = PlanState.requestInProgress;
